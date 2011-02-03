6cf4a9aa1c8e9c69e06eb7603f5f6ef32e80d3f9
Andrew Budarevsky <abudarevsky@gmail.com>
2011-02-04 01:12:13 +0300

    remove all to recreate from webfaction

--- leadmanager/msa_plugin/settings.py
+++
@@ -1,6 +0,0 @@
-# DJANGO IMPORTS
-from django.conf import settings
-from msafactory import post_lead, parse_lead_file
-POST_PROCEDURE = getattr(settings, "MSA_POST_PROCEDURE", post_lead)
-CSV_PARSER = getattr(settings, "MSA_CSV_PARSER", parse_lead_file)
-
